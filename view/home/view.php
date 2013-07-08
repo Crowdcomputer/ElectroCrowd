83279f7c7dffe1a54f02d115f0d986fb9dd90411
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2013-07-08 12:43:14 +0200

    no message

--- view/home/view.php
+++ view/home/view.php
@@ -1,6 +1,6 @@
 <br/>
-<a href="bucket" class="btn">Create a new bucket of resources</a>
-<a href="upload" class="btn">Upload page</a>
-<a href="category" class="btn">Categorization</a>
-<a href="annotation" class="btn">Annotation</a>
-<a href="validate" class="btn">Validation</a>
+<a href="?title=bucket" class="btn">Create a new bucket of resources</a>
+<a href="?title=upload" class="btn">Upload page</a>
+<a href="?title=category" class="btn">Categorization</a>
+<a href="?title=annotation" class="btn">Annotation</a>
+<a href="?title=validate" class="btn">Validation</a>
